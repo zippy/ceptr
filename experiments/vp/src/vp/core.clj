2cdb4efc4114cc91773b5c5fc06edcc8c2e2d70f
Eric Harris-Braun <eric@harris-braun.com>
2013-02-26 15:24:54 -0500

    added process machine experiment

---
+++ experiments/vp/src/vp/core.clj
@@ -0,0 +1,89 @@
+(ns vp.core)
+
+(def instruction-set-words
+  {:restart (fn [state _]
+              (dosync
+               (alter state assoc :pc 0)
+               nil)
+              )
+   :pop (fn [state arguments]
+          (dosync 
+           (let [pc (:pc @state)
+                 mem (:mem @state)]
+             (if (>= pc (count mem)) 
+               nil
+               (let [word (nth mem pc)]
+                 (alter state assoc :pc (+ pc 1))
+                 word)))))})
+
+(def stack-words 
+  {:pop (fn [state _]
+          (dosync (let [s (:stack @state)
+                        val (last s)]
+                    (alter state assoc :stack (pop s))
+                    val)))
+   :push (fn [state arg]
+           (dosync (let [s (:stack @state)]
+                     (alter state assoc :stack (conj s arg))
+                     nil)))})
+
+(def process-words
+  {:process (fn [state arg]
+              (dosync (let [flows @(get @state :flows)
+                            process-flow (if (nil? arg) (get @state :process-flow) arg)
+                            [instruction op1 op2] (tell-flow process-flow :pop nil)
+                            ]
+                        (if (nil? instruction) (str "<Process Completed>")
+                            (cond (= instruction :tell) 
+                                  (let [[flow-id word args] op1]
+                                    (tell-flow flow-id word args)
+                                    )
+                                  (= instruction :link)
+                                  (let [[src-flow-id src-word src-args] op1
+                                        [dst-flow-id dst-word] op2
+                                        dst-args (tell-flow src-flow-id src-word src-args)]
+                                    (tell-flow dst-flow-id dst-word dst-args)
+                                    )
+                                  true (str "<Unknown instruction: " instruction ">"))
+                            )
+                        )))})
+
+(def instructions
+  [[:tell [:screen :push "hello flows"]]
+   [:link [:keyboard :pop] [:stack :push]]
+   [:link [:keyboard :pop] [:stack :push]]
+   [:link [:stack :pop] [:screen :push]]
+   [:link [:stack :pop] [:screen :push]]
+   [:tell [:memory :restart]]])
+
+(def flow-list (ref {}))
+
+(defn tell-flow
+  "say something to a flow in it's protocol"
+  [flow-id word arguments]
+  (let [flow (get @flow-list flow-id)
+        word-fn (word (:words flow))
+        state (:state flow)]
+    (println "telling" flow-id "to" word arguments)
+    (word-fn state arguments)
+    ))
+
+(dosync (alter flow-list assoc 
+               :screen {:words {:push (fn [_ arguments] (println arguments) nil)}}
+               :keyboard {:words {:pop (fn [_ _] (read-line))}}
+               :stack {:words stack-words
+                       :state (ref {:stack []})}                  
+               :memory {:words instruction-set-words
+                        :state (ref {:pc 0 
+                                     :mem instructions})}
+               :_ {:words {:pop (fn [_ _] [:tell [:processor :process]])}}
+               :processor {:words process-words
+                           :state (ref {:process-flow :memory
+                                        :flows flow-list})})
+        )
+
+(defn step
+  "Do one process step"
+  []
+  (tell-flow :processor :process :_))
+
---
+++ experiments/vp/test/vp/core_test.clj
@@ -0,0 +1,7 @@
+(ns vp.core-test
+  (:use clojure.test
+        vp.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
