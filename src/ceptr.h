c69528123b1ac59069a3a5ec4cef868b8d5126d2
Eric Harris-Braun & Lewis Hoffman <eric+lewis@metacurrency.org>
2013-10-28 16:43:37 -0400

    removed bit-level; implemented rough set & get

--- src/ceptr.h
+++ src/ceptr.h
@@ -1,47 +1,132 @@
-#define starting_size 100
+#ifndef _CEPTR_H
+#define _CEPTR_H
 
-enum patternName {
-	BIT,
-	INT,
-	FLOAT	
-};
+#define DEFAULT_ARRAY_SIZE 100
+#define DEFAULT_CACHE_SIZE 10000
+#include <signal.h>
+#include <string.h>
 
-enum nounName {
-	BIT0,BIT1,BIT2,BIT3,BIT4,BIT5,BIT6,BIT7,BIT8,BIT9,BIT10,BIT11,BIT12,BIT13,BIT14,BIT15,BIT16,BIT17,BIT18,BIT19,BIT20,BIT21,BIT22,BIT23,BIT24,BIT25,BIT26,BIT27,BIT28,BIT29,BIT30,BIT31
+enum Symbols {
+	INT,POINT,X,Y,LINE,_LAST
 };	
 
+typedef int Symbol;
+
 typedef struct {
-	int nounName;
-	int patternName;
+	Symbol name;      
+	Symbol patternName;	 //  Symbol patternSpecName;
+} Noun;
+
+typedef struct {
+	Noun *noun;
 	int offset;
-} ElementOffset;
-
+} Offset;
 
 typedef struct {
-	int patternName;
-	ElementOffset children[starting_size];
+	Symbol name;
+	size_t size;
+	Offset children[DEFAULT_ARRAY_SIZE];
 } PatternSpec;
 
+typedef struct {
+	int key;
+	Noun *noun;
+} Xaddr;
 
-PatternSpec specs[starting_size] = {
-	{
-		BIT, {{ 0, 0, 0}}
-	},
-	{
-		INT, 
-		{
-		  {BIT0, BIT, 0},{BIT1 , BIT,  1  },{BIT2 , BIT,  2  },{BIT3 , BIT,  3  },{BIT4 , BIT,  4  },{BIT5 , BIT,  5  },{BIT6 , BIT,  6  },{BIT7 , BIT,  7  },{BIT8 , BIT,  8  },{BIT9 , BIT,  9  },{BIT10, BIT,  10 },{BIT11, BIT,  11 },{BIT12, BIT,  12 },{BIT13, BIT,  13 },{BIT14, BIT,  14 },{BIT15, BIT,  15 },{BIT16, BIT,  16 },{BIT17, BIT,  17 },{BIT18, BIT,  18 },{BIT19, BIT,  19 },{BIT20, BIT,  20 },{BIT21, BIT,  21 },{BIT22, BIT,  22 },{BIT23, BIT,  23 },{BIT24, BIT,  24 },{BIT25, BIT,  25 },{BIT26, BIT,  26 },{BIT27, BIT,  27 },{BIT28, BIT,  28 },{BIT29, BIT,  29 },{BIT30, BIT,  30 },{BIT31, BIT,  31 }
-	    }
-	}
+
+Symbol last_symbol = _LAST;
+
+Noun noun_DATA[DEFAULT_ARRAY_SIZE] = {
+	{ X, INT },
+	{ Y, INT }
 };
 
-ElementOffset e = {BIT0, BIT, 0};
+PatternSpec spec_DATA[DEFAULT_ARRAY_SIZE];
+
+Noun *xaddr_SCAPE[DEFAULT_CACHE_SIZE];
+
+char surfaces_TMP[DEFAULT_CACHE_SIZE];
+
+char cache_DATA[DEFAULT_CACHE_SIZE];
+
+int last_noun = 1;
 
 
+Symbol new_symbol() {
+	return last_symbol++;
+}
+
+void *createSurface(Noun *whatFor) {
+	return surfaces_TMP;
+}
+
+Noun *getNoun(Symbol name){
+	int i;
+	for (i=0; i<DEFAULT_ARRAY_SIZE; i++) {
+		if (noun_DATA[i].name == name) {
+			return &noun_DATA[i];
+		}
+	}
+}
+
+PatternSpec *getPatternSpec(Symbol patternName){
+	int i;
+	for (i=0; i<DEFAULT_ARRAY_SIZE; i++) {
+		if (spec_DATA[i].name == patternName) {
+			return &spec_DATA[i];
+		}
+	}	
+}
+
+int getOffset(PatternSpec *ps, Symbol name) {
+	int i;
+	for (i=0; ps->children[i].noun != 0; i++) {
+		if (ps->children[i].noun->name == name) {
+			return ps->children[i].offset;
+		}
+	}		
+}
 
 
-/*
-	[{}]
+Noun *newNoun(char label[], Symbol patternName) {
+	// FIXME do something useful with label;
+	if ((last_noun +1) == DEFAULT_ARRAY_SIZE) {
+		return 0;
+	}
+	noun_DATA[++last_noun].name = new_symbol();
+	noun_DATA[last_noun].patternName = patternName;
+	return &noun_DATA[last_noun];
+}
+
+
+void* set(Xaddr xaddr, void *value){
+	PatternSpec *ps = getPatternSpec(xaddr.noun->patternName);
+	xaddr_SCAPE[xaddr.key] = xaddr.noun;
+	void *surface = &cache_DATA[xaddr.key];
+	return memcpy(surface, value, ps->size);
+}
+
+void *get(Xaddr xaddr){
+	if (xaddr_SCAPE[xaddr.key] == 0  ||
+		xaddr_SCAPE[xaddr.key]->name != xaddr.noun->name) {
+		return 0;
+	}
+	return &cache_DATA[xaddr.key];
+}
+
+
+void init() { 
+	PatternSpec int_spec = { INT, 4, {{}}};
+	spec_DATA[INT] = int_spec;
 	
+	PatternSpec point_spec = { POINT, 8, {{ getNoun(X), 0 }, { getNoun(Y), 4 }} };
+	spec_DATA[POINT] = point_spec; 
+	// 
+	// 
+	// PatternSpec point_spec = { LINE, 16, {{ getNoun(A), 0 }, { getNoun(B), 8 }} };
+	// spec_DATA[POINT] = point_spec		
+}
 
-*/
+#endif
+
+
--- src/ceptr_spec.c
+++ src/ceptr_spec.c
@@ -1,7 +1,24 @@
 #include "ceptr.h"
 #include <stdio.h>
+#include <signal.h>
 
 int main(int argc, const char** argv)
 {
-	printf("hello, world");
+	init();
+	Noun *here = newNoun("here", POINT);
+	
+	Xaddr hereHere = { 10, here };
+	Xaddr thereHere = { 12, here };
+	
+	// Point p = { x: 22, y: 43 };
+
+	int myPoint[2] = { 2,10 };
+	set(hereHere, &myPoint);
+	void *localHereSurface = get(hereHere);
+	
+	void *failSurface = get(thereHere);
+	
+	printf("failSurface %ld", (long)failSurface);
+	printf("surface-X %d", *(int*)(localHereSurface));
+	printf("surface-Y %d", *(int*)(localHereSurface + 4));
 }
