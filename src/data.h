917a975e1f59a1331d8e29134ad58533308a361f
Eric Harris-Braun <eric@harris-braun.com>
2014-02-25 19:55:42 -0500

    cleanup and refactoring

--- src/data.h
+++ src/data.h
@@ -32,40 +32,6 @@ void size_table_set(Symbol noun, sizeFunction func) {
     size_table[noun] = func;
 }
 
-void *_data_new_uninitialized(Data *d, Xaddr *new_xaddr, Symbol noun, size_t size) {
-    Tnode *n = _t_new(d->root,noun,0,size);
-    new_xaddr->key = _t_children(d->root);
-    new_xaddr->noun = noun;
-    return _t_surface(n);
-}
-
-// this should go somewhere once the dependencies are better resolved.  right now it depends on everything.
-size_t size_of_named_surface(Receptor *r, Symbol instanceNoun, void *surface) {
-    size_t result = 0;
-    if (instanceNoun <= 0){
-        switch (instanceNoun) {
-            case XADDR_NOUN:
-                result = sizeof(Xaddr);
-                break;
-            case CSPEC_NOUN:
-                result = (size_t) 0;
-                break;
-            case CSTRING_NOUN:
-                result = strlen((char *) surface) + 1;
-                break;
-            case PATTERN_SPEC_DATA_NOUN:
-                result = sizeof(PatternSpecData);
-                break;
-            default:
-                raise_error("can't get size of instance of %d \n", instanceNoun);
-        }
-    } else {
-        result = (*size_table_get(spec_noun_for_noun(r, instanceNoun)))(r, instanceNoun, surface);
-    }
-    return result;
-}
-
-
 void data_set(Receptor *r, Xaddr xaddr, void *value, size_t size) {
     Data *d = &r->data;
     void *surface = _data_get(d,xaddr.key);
@@ -73,6 +39,13 @@ void data_set(Receptor *r, Xaddr xaddr, void *value, size_t size) {
         raise_error("I do not think that word (%d) means what you think it means!\n", xaddr.noun);
     }
     memcpy(surface, value, size);
+}
+
+void *_data_new_uninitialized(Data *d, Xaddr *new_xaddr, Symbol noun, size_t size) {
+    Tnode *n = _t_new(d->root,noun,0,size);
+    new_xaddr->key = _t_children(d->root);
+    new_xaddr->noun = noun;
+    return _t_surface(n);
 }
 
 Xaddr data_new(Receptor *r, Symbol noun, void *surface, size_t size) {
@@ -91,15 +64,12 @@ void data_new_by_reference(Receptor *r, Xaddr *new_xaddr, Symbol noun, void *sur
 Symbol data_new_noun(Receptor *r, Xaddr xaddr, char *label) {
     Data *d = &r->data;
     size_t s = sizeof(NounSurface)+strlen(label)+1;
-    Tnode *n = _t_new(d->root,r->nounSpecXaddr.noun,0,s);
-    int key = _t_children(d->root);
-    NounSurface *ns = (NounSurface *)_t_surface(n);
-    ns->specXaddr.key = xaddr.key;
-    ns->specXaddr.noun = xaddr.noun;
+    Xaddr x;
+    NounSurface *ns = _data_new_uninitialized(d,&x,r->nounSpecXaddr.noun,s);
+    ns->specXaddr = xaddr;
     memcpy(&ns->label,label,strlen(label)+1);
-    return key;
+    return x.key;
 }
-
 
 void _data_init(Data *d) {
     scapes_init(d);
@@ -116,4 +86,30 @@ void _data_free(Data *d) {
     _t_free(d->log);
     _t_free(d->root);
     scapes_free(d);
+}
+
+// this should go somewhere once the dependencies are better resolved.  right now it depends on everything.
+size_t size_of_named_surface(Receptor *r, Symbol instanceNoun, void *surface) {
+    size_t result = 0;
+    if (instanceNoun <= 0){
+        switch (instanceNoun) {
+	case XADDR_NOUN:
+	    result = sizeof(Xaddr);
+	    break;
+	case CSPEC_NOUN:
+	    result = (size_t) 0;
+	    break;
+	case CSTRING_NOUN:
+	    result = strlen((char *) surface) + 1;
+	    break;
+	case PATTERN_SPEC_DATA_NOUN:
+	    result = sizeof(PatternSpecData);
+	    break;
+	default:
+	    raise_error("can't get size of instance of %d \n", instanceNoun);
+        }
+    } else {
+        result = (*size_table_get(spec_noun_for_noun(r, instanceNoun)))(r, instanceNoun, surface);
+    }
+    return result;
 }
