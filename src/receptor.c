584f558db8be1ac691b1992f17f1ffb9614799dc
Eric Harris-Braun <eric@harris-braun.com>
2014-08-26 15:12:46 -0400

    fixed doc prob

--- src/receptor.c
+++ src/receptor.c
@@ -1,6 +1,8 @@
 /**
  * @copyright Copyright (C) 2013-2014, The MetaCurrency Project (Eric Harris-Braun, Arthur Brock, et. al).  This file is part of the Ceptr platform and is released under the terms of the license contained in the file LICENSE (GPLv3).
  *
+ * @ingroup receptor
+ *
  * @{
  * @file receptor.c
  * @brief receptor implementation
