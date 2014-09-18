f06be5efe2e5f276b5d6ccac3bb24e2084808cc4
Marc Clifton <marc.clifton@gmail.com>
2014-09-17 21:11:27 -0400

    Part I: Added property grid pane. Added empty symbol editor tree view pane. Added pane layout persistence. Added XTree libs.

--- csharp-ide/Controllers/ApplicationFormController.cs
+++ csharp-ide/Controllers/ApplicationFormController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ using WeifenLuo.WinFormsUI.Docking;
 
 using Clifton.ApplicationStateManagement;
 using Clifton.Assertions;
+using Clifton.ExtensionMethods;
+using Clifton.MycroParser;
 using Clifton.Tools.Strings.Extensions;
 
 using csharp_ide.Actions;
@@ -21,6 +24,8 @@ namespace csharp_ide.Controllers
 	public class ApplicationFormController : ViewController<ApplicationFormView>
 	{
 		public IMruMenu MruMenu { get; protected set; }
+		public SymbolEditorController SymbolEditorController { get; protected set; }
+		public PropertyGridController PropertyGridController { get; protected set; }
 
 		public ApplicationFormController()
 		{
@@ -65,6 +70,7 @@ namespace csharp_ide.Controllers
 
 		protected void Closing(object sender, CancelEventArgs args)
 		{
+			SaveLayout();
 			Program.AppState.SaveState("Form");
 		}
 
@@ -100,7 +106,141 @@ namespace csharp_ide.Controllers
 		{
 		}
 
+		// Pane management
 
+		public void PaneClosed(PaneView pane)
+		{
+			if (pane is SymbolEditorView)
+			{
+				SymbolEditorController = null;
+			}
+			else if (pane is PropertyGridView)
+			{
+				PropertyGridController = null;
+			}
+			else
+			{
+				throw new ApplicationException("Unknown pane : " + pane.GetType().FullName);
+			}
+		}
 
+		protected void ShowPropertyGrid(object sender, EventArgs args)
+		{
+			PropertyGridController.IfNull(() =>
+			{
+				NewDocument("propertyGrid.xml");
+			});
+		}
+
+		protected void ShowSymbolEditor(object sender, EventArgs args)
+		{
+			SymbolEditorController.IfNull(() =>
+			{
+				NewDocument("symbolEditorTree.xml");
+			});
+		}
+
+		protected void LoadTheLayout(string layoutFilename)
+		{
+			View.DockPanel.LoadFromXml(layoutFilename, ((string persistString) =>
+			{
+				string typeName = persistString.LeftOf(',').Trim();
+				string contentMetadata = persistString.RightOf(',').Trim();
+				IDockContent container = InstantiateContainer(typeName, contentMetadata);
+				InstantiateContent(container, contentMetadata);
+
+				return container;
+			}));
+		}
+
+		protected void LoadLayout(object sender, EventArgs args)
+		{
+			if (File.Exists("layout.xml"))
+			{
+				LoadTheLayout("layout.xml");
+			}
+			else
+			{
+				RestoreLayout(sender, args);
+			}
+		}
+
+		protected void RestoreLayout(object sender, EventArgs args)
+		{
+			if (File.Exists("defaultLayout.xml"))
+			{
+				CloseAllDockContent();
+				LoadTheLayout("defaultLayout.xml");
+			}
+		}
+
+		protected void SaveLayout()
+		{
+			// Close documents first, so we don't get dummy documents when we reload the layout.
+			CloseAllDocuments();
+			View.DockPanel.SaveAsXml("layout.xml");
+		}
+
+		protected IDockContent InstantiateContainer(string typeName, string metadata)
+		{
+			IDockContent container = null;
+
+			if (typeName == typeof(GenericPane).ToString())
+			{
+				container = new GenericPane(metadata);
+			}
+			else if (typeName == typeof(GenericDocument).ToString())
+			{
+				container = new GenericDocument(metadata);
+			}
+
+			return container;
+		}
+
+		protected void InstantiateContent(object container, string filename)
+		{
+			MycroParser.InstantiateFromFile<object>(filename, ((MycroParser mp) =>
+			{
+				mp.AddInstance("Container", container);
+				mp.AddInstance("ApplicationFormController", this);
+				mp.AddInstance("ApplicationModel", ApplicationModel);
+			}));
+		}
+
+		protected void CloseAllDockContent()
+		{
+			View.CloseAll();
+		}
+
+		protected void CloseAllDocuments()
+		{
+			View.CloseDocuments();
+		}
+
+		protected void NewDocument(string filename)
+		{
+			GenericDocument doc = new GenericDocument(filename);
+			InstantiateContent(doc, filename);
+			doc.Show(View.DockPanel);
+		}
+
+		protected void NewPane(string filename)
+		{
+			GenericPane pane = new GenericPane(filename);
+			InstantiateContent(pane, filename);
+			pane.Show(View.DockPanel);
+		}
+
+		// Menu management
+
+		public void SetMenuCheckedState(string menuName, bool state)
+		{
+			View.SetMenuCheckState(menuName, state);
+		}
+
+		public void SetMenuEnabledState(string menuName, bool state)
+		{
+			View.SetMenuEnabledState(menuName, state);
+		}
 	}
 }
