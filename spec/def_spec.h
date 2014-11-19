94f1a83941140ac4aa311afb409b3d1b2bf91df0
Eric Harris-Braun <eric@harris-braun.com>
2014-11-18 21:57:20 -0500

    doxygen fix

--- spec/def_spec.h
+++ spec/def_spec.h
@@ -1,5 +1,5 @@
 /**
- * @file symbol_spec.h
+ * @file def_spec.h
  * @copyright Copyright (C) 2013-2014, The MetaCurrency Project (Eric Harris-Braun, Arthur Brock, et. al).  This file is part of the Ceptr platform and is released under the terms of the license contained in the file LICENSE (GPLv3).
  * @ingroup tests
  */
