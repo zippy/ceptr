b5eddb6531df8240d019c763e4d8b92f4f8477c1
Eric Harris-Braun <eric@harris-braun.com>
2015-11-08 18:05:32 -0500

    added a build for stand-alone command-line ceptr app

--- Makefile
+++ Makefile
@@ -1,10 +1,21 @@
-ceptr: clean specs
+all: clean specs ceptr
+.PHONY: all
+
+CEPTR_SRC_FILES := $(wildcard src/*.h src/*.c)
+SPECS_SRC_FILES := $(wildcard src/*.h src/*.c spec/*.c)
+SPECS_SRC_FILES := $(filter-out src/ceptr.c, $(SPECS_SRC_FILES))
+
+ceptr: $(CEPTR_SRC_FILES)
+	gcc -pthread -g -o ceptr $(CEPTR_SRC_FILES)
 
 specs: ceptr_specs
 	./ceptr_specs
 
-ceptr_specs: src/*.h src/*.c spec/*.c
-	gcc -pthread -g -o ceptr_specs spec/*.c src/*.c
+ceptr_specs: $(SPECS_SRC_FILES)
+	gcc -pthread -g -o ceptr_specs $(SPECS_SRC_FILES)
 
 clean:
-	-rm -rf ceptr_specs *.o ceptr_spec.dSYM
+	-rm -rf ceptr_specs *.o ceptr_spec.dSYM ceptr ceptr.dSYM #src/base_defs.c src/base_defs.h
+
+#base_defs.h: src/base_defs
+#	perl src/base_defs.pl
---
+++ src/ceptr.c
@@ -0,0 +1,41 @@
+/**
+ * @file ceptr.c
+ * @copyright Copyright (C) 2013-2015, The MetaCurrency Project (Eric Harris-Braun, Arthur Brock, et. al).  This file is part of the Ceptr platform and is released under the terms of the license contained in the file LICENSE (GPLv3).
+ *
+ * @brief implements main() entry point for the ceptr node
+ */
+
+#include "ceptr.h"
+#include "accumulator.h"
+#include <stdio.h>
+
+int main(int argc, const char **argv) {
+
+    def_sys();
+    char *dname = "tmp/shell_vm";
+
+    //    _a_boot(dname);
+
+    // set up the vmhost
+    G_vm = _v_new();
+    //    _v_instantiate_builtins(G_vm);
+
+    Stream *output_stream, *input_stream;
+    Receptor *i_r,*o_r;
+
+    makeShell(G_vm,stdin,stdout,&i_r,&o_r,&input_stream,&output_stream);
+    output_stream->flags &= ~StreamCloseOnFree; // don't close the stdout on free...
+    input_stream->flags &= ~StreamCloseOnFree; // don't close the stdin on free...
+
+    debug_enable(D_SIGNALS);
+    _v_start_vmhost(G_vm);
+
+    while (G_vm->r->state == Alive) {
+        sleep(1);
+    };
+
+    //    _v_join_thread(&G_vm->clock_thread);
+    _v_join_thread(&G_vm->vm_thread);
+
+
+}
